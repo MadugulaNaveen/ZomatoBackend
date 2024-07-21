f71398bcf9c4de7272f43eb46de7f4fe3e470db1
Naveen Madugula <naveenmadugula7630@gmail.com>
2024-07-21 12:57:08 +0530

    Deploy to vercel

--- ZomatoBackend/settings.py
+++ ZomatoBackend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "rest_framework",
     "restaurants",
     "django.contrib.admin",
@@ -42,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -52,6 +55,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "ZomatoBackend.urls"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3001",
+]
 
 TEMPLATES = [
     {
